01d6eeffcd70b6f660074475092a79edbe92f8b3
Casa Michele <miralo.web@gmail.com>
2017-05-28 15:05:48 +0200

    Provided MySql Connection

---
+++ db.js
@@ -0,0 +1,10 @@
+//DB MYSQL CONNECT 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASS,
+  database : process.env.DB_NAME
+});
+
+module.exports = connection;
