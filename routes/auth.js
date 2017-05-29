8e10d5e7d301014061df269d49c5cd854f145e70
Casa Michele <miralo.web@gmail.com>
2017-05-29 07:57:12 +0200

    Working commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  console.log('Ciao ' + req.body.user + '!')
+
+  db.query('SELECT * FROM users', function(err, rows, fields) {
+    if (err) throw err;
+    rows.forEach(function(row) {
+      if(req.body.user == row.username && req.body.password == row.password) {
+        console.log('The solution is: ', rows);
+      }
+    }, this);
+    
+  });
 });
 
 module.exports = router;
