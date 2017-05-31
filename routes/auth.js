d28eebdb96899fe3d51bd5c90fe2fa9d4d80eef1
Michele <miralo.web@gmail.com>
2017-05-31 18:11:49 +0200

    Work commit

--- routes/auth.js
+++ routes/auth.js
@@ -5,15 +5,17 @@ var db = require('../db');
 /* GET users listing. */
 router.post('/', function(req, res, next) {
 
-  db.query('SELECT * FROM users', function(err, rows, fields) {
-    if (err) throw err;
-    rows.forEach(function(row) {
-      if(req.body.user == row.username && req.body.password == row.password) {
-        console.log('The solution is: ', rows);
-      }
-    }, this);
-    
-  });
+	db.query('SELECT * FROM users', function(err, rows, fields) {
+		if (err) throw err;
+
+		rows.forEach(function(row) {
+			if(req.body.user == row.username && req.body.password == row.password) {
+				console.log('The solution is: ', rows);
+			} else {
+				res.render('index', { error: 'Utente inesistente.' })
+			}
+		}, this);
+	});
 });
 
 module.exports = router;
