557a79f350b5b912b0764e32fe3546a76f4e9504
Casa Michele <miralo.web@gmail.com>
2017-06-01 20:59:05 +0200

    Fixed auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,4 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../db');
-
-/* GET users listing. */
-router.post('/', function(req, res, next) {
-
-	db.query('SELECT * FROM users', function(err, rows, fields) {
-		if (err) throw err;
-
-		rows.forEach(function(row) {
-			if(req.body.user == row.username && req.body.password == row.password) {
-				console.log('The solution is: ', rows);
-			} else {
-				res.render('index', { error: 'Utente inesistente.' })
-			}
-		}, this);
-	});
-});
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express'});
+});
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+
+	db.query('SELECT * FROM users', function(err, rows, fields) {
+		if (err) throw err;
+
+		rows.forEach(function(row) {
+			if(req.body.user == row.username && req.body.password == row.password) {
+				res.redirect(200, '/users');
+			} else {
+				res.render('index', { error: 'Utente inesistente.' })
+			}
+		}, this);
+	});
 });
 
 module.exports = router;
