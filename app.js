a2dc6cfe89a5171b8833d768496e1c533b66bbf1
Casa Michele <miralo.web@gmail.com>
2017-06-03 18:46:01 +0200

    Session middleware installed

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'pug');
 
 var hbs = exphbs.create({
-    defaultLayout: 'main', //we will be creating this layout shortly
+		defaultLayout: 'main', //we will be creating this layout shortly
 });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -36,19 +36,22 @@ app.use(session({secret: 'supercat', saveUninitialized: true, resave: true}));
 
 // Session-persisted message middleware
 app.use(function(req, res, next){
-  var err = req.session.error,
-      msg = req.session.notice,
-      success = req.session.success;
+	var err = req.session.error,
+		msg = req.session.notice,
+		win = req.session.success;
+		log = req.session.logged;
 
-  delete req.session.error;
-  delete req.session.success;
-  delete req.session.notice;
+	delete req.session.error;
+	delete req.session.success;
+	delete req.session.notice;
+	delete req.session.logged;
 
-  if (err) res.locals.error = err;
-  if (msg) res.locals.notice = msg;
-  if (success) res.locals.success = success;
+	if (err) res.locals.error = err;
+	if (msg) res.locals.notice = msg;
+	if (win) res.locals.success = win;
+	if (log) res.locals.logged = logged;
 
-  next();
+	next();
 });
 
 app.use('/', index);
@@ -57,20 +60,20 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
