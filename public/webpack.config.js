625c31567d781e0defe5c78f5a5459e4be342db2
Casa Michele <miralo.web@gmail.com>
2017-06-23 09:50:09 +0200

    Piccoli fix

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -2,11 +2,13 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: {
+    dashboard: './src/dashboard.js'
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: '[name].entry.js'
   },
   module: {
     rules: [
