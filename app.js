1eb7bb06e983225e032e5ee1997f5b0a3ca247df
Michele <miralo.web@gmail.com>
2017-06-27 18:05:14 +0200

    Cambiata la callback nello strategy

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var strategy = new Auth0Strategy({
 	domain:       process.env.AUTH0_DOMAIN,
 	clientID:     process.env.AUTH0_CLIENT_ID,
 	clientSecret: process.env.AUTH0_CLIENT_SECRET,
-	callbackURL:  'http://localhost:3000/callback'
+	callbackURL:  'https://pure-anchorage-13843.herokuapp.com/callback'
 }, function(accessToken, refreshToken, extraParams, profile, done) {
 	// profile has all the information from the user
 	return done(null, profile);
